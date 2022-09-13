c48f0fa4e2ef379832de5060df70082b928e62be
Lucca Rendall <luccarendall2@gmail.com>
2022-09-13 08:56:33 -0300

    Depois de horas de humilhação no backend estou de volta

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 require('dotenv').config();
 
-// const DEFAULT_TIMEOUT = 30000;
+const DEFAULT_TIMEOUT = process.env.REACT_APP_HAWKINS_TIMEOUT;
 class CharactersService {
   constructor({
     url = process.env.REACT_APP_HAWKINS_URL,
-    timeout = process.env.REACT_APP_HAWKINS_TIMEOUT,
+    timeout = DEFAULT_TIMEOUT,
   }) {
     this.http = axios.create({
       baseURL: url,
