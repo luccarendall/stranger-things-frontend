29d9262964b9737082e3da3f3482de5ea6479bcc
Lucca Rendall <luccarendall2@gmail.com>
2022-09-13 04:43:59 -0300

    app pronto

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
-const DEFAULT_TIMEOUT = 30000;
+require('dotenv').config();
+
+// const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
+  constructor({
+    url = process.env.REACT_APP_HAWKINS_URL,
+    timeout = process.env.REACT_APP_HAWKINS_TIMEOUT,
+  }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
